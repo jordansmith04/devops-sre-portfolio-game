1757fb1c04577def0cb2ef71d7a89128cbff5782
Jordan Smith <jordan.smith@peraton.com>
2025-10-29 15:18:05 -0700

    Updating Github deploy yaml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/devops-sre-portfolio-game/',
   plugins: [react()],
-  base: "/devops-sre-portfolio-game/",
 })
