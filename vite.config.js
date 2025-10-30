3d35b5099bb39b6bf955bc2a41d4f17ee6688f59
Jordan Smith <jordan.smith@peraton.com>
2025-10-30 08:14:00 -0700

    Figuring out this deploy process

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/devops-sre-portfolio-game/',
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'serve' ? '/' : '/devops-sre-portfolio-game/', // ✅ Adjust for GitHub Pages
   build: {
     outDir: 'dist',
   },
-})
+}))
